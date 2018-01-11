08c22d9de3f92f0432c0cb9b0a7d3863ef7c912e
buip <prestonbui97@gmail.com>
2018-01-11 16:51:44 +0700

    Add more info into user model and test

--- server/server.js
+++ server/server.js
@@ -93,6 +93,13 @@ app.patch('/todos/:id', (req, res) => {
   }).catch(e => res.status(400).send());
 });
 
+app.post('/users', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+  let user = new User(body);
+  user.save().then(user => {
+    res.send(user);
+  }, error => res.status(400).send(error));
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
